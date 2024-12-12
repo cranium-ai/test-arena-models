56e216ef94c5fc8ae887b5b6a3d03d30b8d40c5f
Paul Brinson <pbrinson@cranium.ai>
2024-12-12 15:01:54 -0400

    Added code to load some models.

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from transformers import AutoModel
+
+print("Loading models...")
+
+model1 = AutoModel.from_pretrained("google-bert/bert-base-cased")
+model2 = AutoModel.from_pretrained("google/gemma-2-27b")
+model3 = AutoModel.from_pretrained("HuggingFaceTB/SmolLM2-1.7B")
+model4 = AutoModel.from_pretrained("meta-llama/Llama-2-13b-chat-hf")
+model5 = AutoModel.from_pretrained("microsoft/Phi-3.5-mini-instruct")
+model6 = AutoModel.from_pretrained("nvidia/Llama-3.1-Nemotron-70B-Instruct-HF")
+
+print("Models loaded successfully!")
