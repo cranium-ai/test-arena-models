9a468d7dc2a03715505cd4fc5adc0480989d6ac1
Paul Brinson <157505660+cranium-paulb@users.noreply.github.com>
2025-06-03 15:11:55 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -10,6 +10,8 @@ model5 = AutoModel.from_pretrained("microsoft/Phi-3.5-mini-instruct")
 model6 = AutoModel.from_pretrained("nvidia/Llama-3.1-Nemotron-70B-Instruct-HF")
 model8 = AutoModel.from_pretrained("meta-llama/Llama-3.2-1B")
 model9 = AutoModel.from_pretrained("facebook/convnext-base-224-22k-1k")
+model10 = AutoModel.from_pretrained("sentence-transformers/all-MiniLM-L12-v2")
+model11 = AutoModel.from_pretrained("hf-internal-testing/tiny-random-LlamaForCausalLM")
 
 print("Models loaded successfully!")
 
