7e583c964046a44d19d27bdc71425719eacebfe6
ekim1394 <ekim@cranium.ai>
2025-08-27 17:08:17 -0400

    use smollm2-135m

--- src/main.py
+++ src/main.py
@@ -2,18 +2,7 @@ from transformers import AutoModel
 
 print("Loading models...")
 
-model1 = AutoModel.from_pretrained("google-bert/bert-base-cased")
-#model2 = AutoModel.from_pretrained("google/gemma-2-27b")
-model3 = AutoModel.from_pretrained("HuggingFaceTB/SmolLM2-1.7B")
-model4 = AutoModel.from_pretrained("meta-llama/Llama-2-13b-chat-hf")
-model5 = AutoModel.from_pretrained("microsoft/Phi-3.5-mini-instruct")
-model6 = AutoModel.from_pretrained("nvidia/Llama-3.1-Nemotron-70B-Instruct-HF")
-model8 = AutoModel.from_pretrained("meta-llama/Llama-3.2-1B")
-model9 = AutoModel.from_pretrained("facebook/convnext-base-224-22k-1k")
-model10 = AutoModel.from_pretrained("sentence-transformers/all-MiniLM-L12-v2")
-model11 = AutoModel.from_pretrained("hf-internal-testing/tiny-random-LlamaForCausalLM")
-model12 = AutoModel.from_pretrained("openai/gpt-oss-20b")
-
+model3 = AutoModel.from_pretrained("HuggingFaceTB/SmolLM2-135M")
 
 print("Models loaded successfully!")
 
